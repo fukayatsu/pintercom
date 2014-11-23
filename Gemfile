d672cb6a93b7c23a7aab5c489ecbfbb613d377ae
fukayatsu <fukayatsu@gmail.com>
2014-11-23 05:07:34 +0000

    Use idobata instead of hipchat

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source "https://rubygems.org"
 
+ruby '2.1.5'
+
 gem 'pi_piper'
 gem 'faraday'
 gem 'dotenv'
-gem 'hipchat'
-#gem 'pry'
+gem 'idobata_hook'
+gem 'pry'
 
--- notifier.rb
+++ notifier.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
-require 'hipchat'
+require 'idobata_hook'
 require 'pi_piper'
 require 'json'
 require 'faraday'
 require 'dotenv'
-#require 'pry'
+# require 'pry'
 
 Dotenv.load
 
@@ -13,65 +13,28 @@ Dotenv.load
 
 class Notifier
   def initialize
-    @hipchat = HipChat::Client.new(ENV['HIPTCHAT_API_TOKEN'])
-    @http    = http
-    @led     = PiPiper::Pin.new pin: 17, direction: :out
-    @buzzer  = PiPiper::Pin.new pin: 4,   direction: :out
-  end
-
-  def http
-    Faraday.new(url: ENV['HEROCKER_URL']) do |f|
-      f.request :multipart
-      f.request :url_encoded
-      f.adapter :net_http 
-    end
-  end
-
-  def beep(sec)
-      @buzzer.on
-      sleep sec
-      @buzzer.off
+    @idobata = IdobataHook::Client.new(ENV['IDOBATA_HOOK_URL'])
   end
 
   def watch_pin
     PiPiper.after pin: 24, goes: :down  do |pin|
       Thread.new do
-        @led.on; beep(0.1); @led.off
         puts "[#{Time.now}] button pressed"
 
         `cd /home/pi/pintercom; raspistill -t 1 -o tmp/pic.jpg -w 1280 -h 800 -q 80`
 
-        beep(0.1); sleep 0.1; beep(0.1)
-
-        @hipchat['entrance'].send('pintercom', "@here Someone has come!", message_format: 'text')
-        image_url = upload('tmp/pic.jpg')
-        @hipchat['entrance'].send('pintercom', image_url, message_format: 'text')
-
-        @led.on; sleep(0.1); @led.off
+        @idobata.send(ENV['IDOBATA_MESSAGE'], format: :text, image_path: 'tmp/pic.jpg')
       end
     end
-    `echo high > /sys/class/gpio/gpio24/direction` # pull-up
   end
 
   def start
+    `echo "24" > /sys/class/gpio/unexport`
     watch_pin
+    `echo high > /sys/class/gpio/gpio24/direction` # pull-up
+
     puts "[#{Time.now}] start"
-    beep(0.1)
     PiPiper.wait
-  end
-
-  def upload(image_file)
-    payload = {
-      file: Faraday::UploadIO.new(image_file, 'image/jpeg'),
-      upload_token: ENV['HEROCKER_UPLOAD_TOKEN'],
-    }
-    res = @http.post '/images.json', payload
-
-    image_url = JSON.parse(res.body)["image_url"]
-
-  rescue => e
-    p e.message
-    p "file upload error"
   end
 end
 
